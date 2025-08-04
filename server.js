a627442305f4c03e4309fd6d85daa6f9fe1c140e
genius24 <amitya9854@gmail.com>
2025-08-04 13:02:52 +0530

    Adding Cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from "express";
-import fs from "fs";
 import dotenv from "dotenv";
-
+import fs from "fs";
+import cors from "cors"
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 const filePath = process.env.DATA_FILE || "./JsonFileData/Data.json";
@@ -18,7 +19,7 @@ app.get("/api", (req, res) => {
     try {
       const parsed = JSON.parse(data);
       res.json(parsed);
-     
+
     } catch (err) {
       res.status(500).json({ error: "Invalid JSON format in file." });
     }
